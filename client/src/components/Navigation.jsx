e089d9590dbd88736506d47ffa38a809eb9cc80b
Kayla Justice <kaylarjustice@gmail.com>
2022-11-25 11:55:44 -0600

    Editing text in src files to match project specifications

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -11,8 +11,8 @@ const Navigation = (props) => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/products">Products</Nav.Link>
-            <Nav.Link href="/users">Users</Nav.Link>
+            <Nav.Link href="/products">Quizzes</Nav.Link>
+            <Nav.Link href="/users">Profile</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
