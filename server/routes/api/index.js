772b7d36b0b1df77cc5f31ae078455e5bbca19df
Kayla Justice <kaylarjustice@gmail.com>
2022-11-28 09:07:23 -0600

    Adding reviewRoutes to api index.js file

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const reviewRoutes = require('./review-routes');
 
 router.use('/user', userRoutes);
+
+/**
+ * Are we treating reviews similar to posts, in that they can be searched by quiz and user?
+ */
+
+router.use('/review', reviewRoutes);
+
 module.exports = router;
