882930c74f05e621a064b66847de6a60f8fc6d2e
Julia <juliafritsch04@gmail.com>
2022-11-29 13:45:30 -0600

    home-page-style

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,19 +1,21 @@
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
+import logo from "../assets/logo.png"
+
 
 const Navigation = (props) => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="light" variant="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">My Site</Navbar.Brand>
+        <Navbar.Brand href="#home"><img className="logo" src={logo} alt="buzzbuzz logo" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/products">Products</Nav.Link>
-            <Nav.Link href="/users">Users</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/Quizzes">Quizzes</Nav.Link>
+            <Nav.Link href="/Profile">Profile</Nav.Link>
+            <Nav.Link href="/Login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
